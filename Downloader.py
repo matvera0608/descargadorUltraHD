5feb8b509db5847832d878f846884c0dcc18d322
Vera_Ramiro <veram0227@gmail.com>
2025-10-02 18:39:04 -0300

    Correcciones y optimización del código.

--- Downloader.py
+++ Downloader.py
@@ -14,52 +14,60 @@ def optar(url):
     
     print("\nOpciones de calidad: \n")
     print("1 - Mejor calidad disponible (video + audio)")
-    # print("2 - Mejor calidad hasta 1080p (evita 4K)")
     print("2 - Descargar sólo el sonido de la mejor calidad")
     print("3 - Elegir manualmente de la lista")
     
     opción = input("\nIntroduce el código de formato deseado: ").strip()
-    
+
     match opción:
         case "1":
             return (
-            "bestvideo+bestaudio/best",
-            [
-                {"key": "FFmpegVideoRemuxer", "preferedformat": "mp4"}
-            ],
+                "bestvideo+bestaudio/best",
+                [
+                    {"key": "FFmpegVideoRemuxer", "preferedformat": "mp4"},
+                ],
             )
-        # case "2":
-        #     return "bestvideo[height<=1080]+bestaudio/best[height<=1080]"
         case "2":
             return (
-            "bestaudio/best",
-            [
-                {"key": "FFmpegExtractAudio", "preferredcodec": "aac", "preferredquality": "192"}
-            ],
+                "bestaudio/best",
+                [
+                    {"key": "FFmpegExtractAudio", "preferredcodec": "aac", "preferredquality": "192"},
+                ],
             )
         case "3":
             listarCalidadesSegúnPágina(url)
             formato = input("\nIntroduce el código de formato deseado: ")
-            return formato
+            return formato, []
         case _:
             print("\nOpción inválida, se seleccionará la mejor calidad disponible por defecto.")
-            return ("bestvideo+bestaudio/best",
-            [
-                {"key": "FFmpegVideoRemuxer", "preferedformat": "mp4"}
-            ],
+            return (
+                "bestvideo+bestaudio/best",
+                [
+                    {"key": "FFmpegVideoConvertor", "preferedformat": "mp4" }
+                ],
             )
 
-def descargar_subtítulos():
+
+def descargar_subtítulos(url):
     opción = input("¿Querés descargar los subtítulos? ").lower().strip()
-    if opción in ["si", "s"]:
+    def listar_lenguas(url):
+        ydl_opts_info = {"listsubtitles": True}
+        with YoutubeDL(ydl_opts_info) as ydl:
+            ydl.extract_info(url, download=False)
+    if opción in ["si", "s", "sí"]:
+        listar_lenguas(url)
+        if "bilibili" in url:
+            idioma = "zh-Hans"
+        else:
+            idioma = input("¿En qué idioma está el video? ").lower().strip() or "es"
+
         return {
         "writesubtitles": True,
         "writeautomaticsub": True,
-        "subtitleslangs": ["all"],
+        "subtitleslangs": [idioma],
         "subtitlesformat": "srt",
     }
     else:
-        print("Se descarga sin subtítulos.")
         return {}
 
 def descargar():
@@ -72,23 +80,26 @@ def descargar():
         while "http" not in url or url.strip() == "":
             print("\n URL inválida, por favor introduce una URL válida.")
             url = obtenerURL()
-        formato = optar(url)
-        print(f"\nDescargando video {cant + 1} de {cant_video}...")
-        subtítulos = descargar_subtítulos()
+        formato , procesoCodificación = optar(url)
+        
         ydl_opts = {
             "outtmpl": r"C:\Users\veram\Downloads\%(title)s.%(ext)s",
             "format": formato,
             "merge_output_format": "mp4",
             "noplaylist": True,
             "nooverwrites": True,
-            "postprocessors": [{
-            "key": "FFmpegVideoRemuxer",
-            "preferedformat": "mp4"
-            }]
-        }
+            "postprocessors": procesoCodificación,
+            "postprocessor_args": [
+                "-c:v", "copy",
+                "-c:a", "aac",
+                "-b:a", "192k" 
+                ],
+            }
+        subtítulos = descargar_subtítulos(url)
         if isinstance(subtítulos, dict):
             ydl_opts.update(subtítulos)
         try:
+            print(f"\nDescargando video {cant + 1} de {cant_video}...")
             with YoutubeDL(ydl_opts) as ydl:
                     ydl.download([url])
                     print("\n Video descargado COMPLETAMENTE, QUE SASTISFACTORIO.\n")
